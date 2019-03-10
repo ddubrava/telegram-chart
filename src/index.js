6d7d67bbe18b3984f465eee0d5d6bd1807fbcf24
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-03-11 00:48:43 +0300

    chore: move mode.js in separated file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './assets/styles/style.scss';
 import './assets/styles/index.scss';
 
+require('./scripts/mode.js');
 require('./scripts/app.js');
---
+++ src/scripts/mode.js
@@ -0,0 +1,19 @@
+const changeMode = () => {
+  const main = document.querySelector('.main');
+  const span = document.querySelector('.main__switch-mode');
+  let mode = 0; // 0 - day, 1 - night
+
+  span.addEventListener('click', () => {
+    main.classList.toggle('main_midnight-blue');
+
+    if (mode) {
+      span.innerHTML = 'Switch to Night Mode';
+      mode = 0;
+    } else {
+      span.innerHTML = 'Switch to Day Mode';
+      mode = 1;
+    }
+  });
+};
+
+changeMode();
