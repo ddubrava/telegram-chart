c718ae0d0764eb95ea1b58d9f92535c391e4b788
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-03-10 03:42:28 +0300

    chore: add file-loader for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,15 @@ module.exports = {
           loader: 'babel-loader'
         }
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }]
+      }
     ]
   },
 
