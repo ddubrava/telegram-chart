02b450ea0274ea3567a4e8f6563d1e1d17b1ce23
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-03-11 01:26:18 +0300

    chore(app.js): update chart styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -66,21 +66,23 @@ const drawGrid = chart => {
     ctx.beginPath();
     ctx.moveTo(x, y);
     ctx.lineTo(canvas.width, y);
+    ctx.strokeStyle = '#DFE6EB';
     ctx.stroke();
   };
 
   const drawXValues = (x, y, i) => {
-    ctx.fillText(xValues[i], x + 5, y + canvas.height / 6 - 5);
+    ctx.fillStyle = '#9DA8B0';
+    ctx.fillText(xValues[i - 1], x + 5, y - 5);
   };
 
   const drawYValues = (x, y, i) => {
-    ctx.fillText(yValues[i], x + 15, y - 5);
+    ctx.fillText(yValues[i - 1], x + 10, y);
   };
 
-  for (let i = 0; i < 6; i += 1) {
-    drawXLines(0, canvas.height / 6 * i);
-    drawXValues(0, canvas.height / 6 * i, i);
-    drawYValues(canvas.width / 6 * i, canvas.height, i);
+  for (let i = 1; i < 7; i += 1) {
+    drawXLines(0, canvas.height / 6 * i - 20);
+    drawXValues(0, canvas.height / 6 * i - 20, i);
+    drawYValues(canvas.width / 6 * (i - 1), canvas.height, i);
   }
 };
 
