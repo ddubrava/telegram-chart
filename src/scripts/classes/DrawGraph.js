d5f536d8314debee80376f57bc6c5ac0f4ea3a3e
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-03-14 21:21:20 +0300

    feat(DrawGraph.js): update drawLines() for drawng multiple lines at same time

--- src/scripts/classes/DrawGraph.js
+++ src/scripts/classes/DrawGraph.js
@@ -4,37 +4,40 @@ export default class DrawGraph {
     this.ctx = ctx;
     this.canvasActualHeight = canvasActualHeight;
     this.heightOffset = heightOffset;
-    this.chart = chart;
     this.minValue = minValue;
     this.maxValue = maxValue;
-    this.drawLine(1);
-    this.drawLine(2);
+    this.drawLines(chart);
 
     emitter.subscribe('event:scale-change', data => {
-      this.drawLine(1, data);
-      this.drawLine(2, data);
+      this.drawLines(chart, data);
     });
   }
 
-  drawLine(chartNumber, scale = 25) {
-    const lineYCoordinates = [];
-    this.chart.columns[chartNumber].forEach((item, i) => {
-      if (i > 0) {
-        lineYCoordinates.push(
-          this.canvasActualHeight - (item - this.minValue)
-            * (this.canvasActualHeight / (this.maxValue - this.minValue))
-              + this.heightOffset
-        );
+  drawLines(chart, scale = 25) {
+    chart.columns.forEach((col, index) => {
+      if (index > 0) {
+        const colName = col[0];
+        const lineYCoordinates = [];
+
+        col.forEach((item, i) => {
+          if (i > 0) {
+            lineYCoordinates.push(
+              this.canvasActualHeight - (item - this.minValue)
+                * (this.canvasActualHeight / (this.maxValue - this.minValue))
+                  + this.heightOffset
+            );
+          }
+        });
+
+        for (let i = 0; i < lineYCoordinates.length - 1; i += 1) {
+          this.ctx.beginPath();
+          this.ctx.moveTo(this.canvas.width / scale * i + 15, lineYCoordinates[i]);
+          this.ctx.lineTo(this.canvas.width / scale * [i + 1] + 15, lineYCoordinates[i + 1]);
+          this.ctx.lineWidth = 2;
+          this.ctx.strokeStyle = chart.colors[colName];
+          this.ctx.stroke();
+        }
       }
     });
-
-    for (let i = 0; i < lineYCoordinates.length - 1; i += 1) {
-      this.ctx.beginPath();
-      this.ctx.moveTo(this.canvas.width / scale * i + 15, lineYCoordinates[i]);
-      this.ctx.lineTo(this.canvas.width / scale * [i + 1] + 15, lineYCoordinates[i + 1]);
-      this.ctx.lineWidth = 2;
-      this.ctx.strokeStyle = chartNumber === 1 ? this.chart.colors.y0 : this.chart.colors.y1;
-      this.ctx.stroke();
-    }
   }
 }
