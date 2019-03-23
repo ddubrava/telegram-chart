60fef2793e37950e185d90bfb76bdb3e58d0443f
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-03-23 10:16:06 +0300

    chore: remove file-loader from package, config

--- webpack.config.js
+++ webpack.config.js
@@ -26,15 +26,6 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]'
-          }
-        }]
       }
     ]
   },
